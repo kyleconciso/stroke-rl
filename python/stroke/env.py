75c9c53e49810280b4ce265d5b34c37463e716d4
kyleconciso <kyleconciso@gmail.com>
2025-02-17 15:22:53 +0800

    penalize idle state

--- python/stroke/env.py
+++ python/stroke/env.py
@@ -68,7 +68,8 @@ class StrokeEnv(gym.Env):
             reward = (-delta_change*100)
             # terminated = True
         elif delta_change ==0 and self._step>0:
-            reward = 0
+            reward = -100
+            terminated = True
         elif delta_change < 0:
             reward = -delta_change*100
 
